588aebdfdb65bfaa86e70e62033abbdaf8806737
Imad Antonios <8104351+iantonios@users.noreply.github.com>
2026-08-12 13:20:57 -0400

    Add selectbox and slider to sidebar
    Added a selectbox and a slider to the sidebar for user input.

--- st-tabs.py
+++ st-tabs.py
@@ -26,3 +26,15 @@ with tab1:
 
 with tab2:
    st.pyplot(fig2)
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
